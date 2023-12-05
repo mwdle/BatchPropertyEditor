daffef2af130b5908887ed6f870d9b5cbf918d87
Michael Wadley <michael@mwdle.com>
2023-12-05 01:04:35 -0700

    Project complete

---
+++ readme.md
@@ -0,0 +1,28 @@
+
+# Bulk Property File Editor
+#### Author: Michael Wadley
+#### Date: 12/05/2023
+
+## Purpose:
+This console application aims to simplify and significantly hasten the process of changing a specific property value across many property files.
+
+## Definitions:
+* Property File: In the context of this application, a property file is a test file containing key-value pairs that are used to configure a program (such as an AFT).
+  * Key value pairs are written in the format of key=value, and are listed one per line.
+* Deep search: A type of search that looks for a file in a folder and all of its sub-folders.
+
+## How To Use:
+
+#### The application has informative prompts to guide you through using it, but if you are unsure of something you can always refer to this section.
+
+* Upon opening, you are prompted to specify a folder (ex: C:\folderContainingAFTs) to do a deep search for property files in.
+  * If you input an invalid directory path, you will be warned of an error and prompted to enter a valid directory.
+  * **Warning:** This application is designed to work on Windows, this means all directory paths must be specified using backslashes (\) instead of forward slashes (/). Application behavior with forward slashes is undefined and may result in errors.
+* Then you will be prompted to enter the common name shared between the property files you want to modify (ex: aft.properties).
+* Then you will be prompted to decide the file modification mode. There are two options:
+  * Setting the value of a given key. This option means you must specify a key and a value. For example, you could specify a key 'username' and a value 'dave', then each property file containing the key 'username' would have its value set to 'dave'.
+  * Setting a value given the old value. This option means you must specify an old value and a new value. For example, you could specify an old value 'dave' and a new value 'david', then each property file containing the old value 'dave' would have its value set to 'david'.
+    * **Warning:** a property file can have multiple keys with the same value. This option will change all keys with the old value to the new value. Be cautious with this.
+* Then, depending on which option you selected in the last step, you will be prompted to enter either a key or an old value.
+* Lastly, you will be prompted to enter the new value you wish to set in the property files.
+* The application will then perform a deep search for property files that match the name you specified in the directory you specified. It will then modify the property files according to the mode you specified, and output the name and path of each modified file as well as how many files were modified.
---
+++ src/main/java/BulkPropertyEditor.java
@@ -0,0 +1,122 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class BulkPropertyEditor {
+
+    public static int numberOfFilesModified = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Welcome to the Bulk Property File Editor!");
+        System.out.println("To get started, please enter the absolute path to your folder of AFT's (NOT case sensitive) (ex: C:\\Users\\Dave\\folderOfAFTs):");
+        System.out.println("NOTE: forward slashes have undefined behavior. Please follow the windows filesystem convention of using backslashes.");
+        String aftFolder = reader.readLine();
+
+        while (!aftFolder.matches("^(C|c):\\\\(.+ *\\\\?)+$")) {
+            System.out.println("Invalid path entered. Please enter a valid absolute path to your folder of AFT's (ex: C:\\Users\\Dave\\folderOfAFTs):");
+            aftFolder = reader.readLine();
+        }
+        if (aftFolder.charAt(aftFolder.length() -1) == '\\')
+            aftFolder = aftFolder.substring(0, aftFolder.length() - 1);
+
+        // Create a File object for the directory
+        File aftDirectory = new File(aftFolder);
+        // Check if the provided path is a directory
+        if (!aftDirectory.isDirectory()) {
+            System.out.println("The directory path you provided was not found. Please rerun the program and try again.");
+            System.exit(0);
+        }
+
+        System.out.println();
+        System.out.println("Please enter the common name of the property files (ex: aft.properties):");
+        String propertyFileName = reader.readLine();
+
+        System.out.println();
+        System.out.println("There are two file modification modes:");
+        System.out.println("1. Setting the value of a given key. This option means you must specify a key and a value. For example, you could specify a key 'username' and a value 'dave', then each property file containing the key 'username' would have its value set to 'dave'.");
+        System.out.println("2. Setting a value given the old value. This option means you must specify an old value and a new value. For example, you could specify an old value 'dave' and a new value 'david', then each property file containing the old value 'dave' would have its value set to 'david'.");
+        System.out.println("NOTE: option 2 can be dangerous if a file has multiple keys with the same value, as they will all be replaced. Be cautious with this option.");
+        System.out.println("Please enter the modification mode you would like to use (1 or 2):");
+        String option = reader.readLine();
+        while (!option.equals("1") && !option.equals("2")) {
+            System.out.println("Invalid option entered. Please enter either 1 or 2:");
+            option = reader.readLine();
+        }
+
+        String propKey = "";
+        String oldPropValue = "";
+        System.out.println();
+
+        switch(option)
+        {
+            case "1" -> {
+                System.out.println("Please enter the key to replace the value for (ex: username):");
+                propKey = reader.readLine();
+            }
+            case "2" -> {
+                System.out.println("Please enter the old property value to replace (ex: dave):");
+                oldPropValue = reader.readLine();
+            }
+        }
+
+        System.out.println();
+        System.out.println("Please enter the new property value to set (ex: david):");
+        String propNewValue = reader.readLine();
+        System.out.println();
+
+        if (option.equals("1"))
+            replacePropertyRecursive(aftDirectory, propertyFileName, propKey, propNewValue, true);
+        else
+            replacePropertyRecursive(aftDirectory, propertyFileName, oldPropValue, propNewValue, false);
+
+        System.out.println();
+        System.out.println("Modification complete. Successfully modified " + numberOfFilesModified + " files. Please check one of the property files to make sure the property was set correctly.");
+    }
+
+    private static void replacePropertyRecursive(File directory, String propFileName, String propKeyOrOldValue, String newPropValue, boolean replaceByKey) {
+        // Get all files and subdirectories in the current directory
+        File[] files = directory.listFiles();
+
+        if (files != null && !directory.getName().equals(".git")) {
+            for (File file : files) {
+                // If it's a directory, then recursively search in that directory
+                if (file.isDirectory()) {
+                    replacePropertyRecursive(file, propFileName, propKeyOrOldValue, newPropValue, replaceByKey);
+                } else {
+                    // Check if the current file matches the desired file name
+                    if (file.getName().equals(propFileName)) {
+                        try {
+                            if (replaceByKey)
+                                replaceTextInFile(file, propKeyOrOldValue + " *= *(.+)", propKeyOrOldValue + "=" + newPropValue + "\n");
+                            else replaceTextInFile(file, propKeyOrOldValue, newPropValue);
+                        } catch (IOException e) {
+                            System.out.println("Unknown error occurred writing to file. Please restart the program and try again.");
+                            System.exit(0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void replaceTextInFile(File file, String oldText, String newText) throws IOException {
+        Path path = Paths.get(file.getAbsolutePath());
+        Charset charset = StandardCharsets.UTF_8;
+        String oldContent = Files.readString(path, charset);
+        String newContent = oldContent.replaceAll(oldText, newText);
+        if (!oldContent.equals(newContent)) {
+            numberOfFilesModified++;
+            System.out.println("Modified file: " + file.getAbsolutePath());
+        }
+        Files.writeString(path, newContent, charset);
+    }
+}
