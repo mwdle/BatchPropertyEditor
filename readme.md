7fb62cd0a254b86f95367ffd9806aaae57c72918
mwdle <michael@mwdle.com>
2024-06-20 12:40:09 -0600

    Updated with undo functionality. Minor code refactor. Updated and improved documentation. Improved regex matching and validation of user input.

--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 # Batch Property Editor
 #### Author: Michael Wadley
 #### Date: 12/05/2023
+#### Updated: 6/20/24
 
 ## Purpose:
 This interactive console application aims to simplify and significantly hasten the process of changing a specific property value in many property files at once with just a few keystrokes.
@@ -19,15 +20,18 @@ This interactive console application aims to simplify and significantly hasten t
   * If you input an invalid directory path, you will be warned of an error and prompted to enter a valid directory.
   * **Warning:** This application is designed to work on Windows, this means all directory paths must be specified using backslashes (\) instead of forward slashes (/). Application behavior with forward slashes is undefined and may result in errors.    <br><br>
   
-* Then you will be prompted to enter the common name of the property files or a regular expression to match by (ex: user.properties to modify files called user.properties or .+.properties to modify files ending with .properties).    <br><br>
+* You will then be prompted to enter the common name shared between the property files you want to modify (ex: aft.properties).
+  * Supports regex entry such as: .*\.properties (to match any file ending in .properties) or aft.properties (to match any file with exact name 'aft.properties')    <br><br>
 
 * Then you will be prompted to decide the file modification mode. There are two options:
-  * Setting the value of a given key. This option means you must specify a key and a value. For example, you could specify a key 'username' and a value 'dave', then each property file containing the key 'username' would have its value set to 'dave'.
-  * Setting a value given the old value. This option means you must specify an old value and a new value. For example, you could specify an old value 'dave' and a new value 'david', then each property file containing the old value 'dave' would have its value set to 'david'.
-    * **Warning:** a property file can have multiple keys with the same value. This option will change all keys with the old value to the new value. Be cautious with this.    <br><br>
+  * Setting the value of a given key. This option means you must specify a key and a value. For example, you could specify a key 'db.password' and a value 'myDbPassword', then each property file containing the key 'db.password' would have its value set to 'myDbPassword'.
+  * Setting a value given the old value. This option means you must specify an old value and a new value. For example, you could specify an old value 'myOldDbPassword' and a new value 'myNewDbPassword', then each property file containing the old value 'myOldDbPassword' would have that value replaced with 'myNewDbPassword'.
+    * **Warning:** a property file can have multiple keys with the same value. This option changes all instances of the old value to the new value. Be cautious with this.    <br><br>
     
 * Then, depending on which option you selected in the last step, you will be prompted to enter either a key or an old value.    <br><br>
 
 * Lastly, you will be prompted to enter the new value you wish to set in the property files.    <br><br>
 
-* The application will then perform a deep search for property files that match the name you specified in the directory you specified. It will then modify the property files according to the mode you specified, and output the name and path of each modified file as well as how many files were modified.
+* The application will then perform a deep search for property files that match the name you specified in the directory you specified. It will then modify the property files according to the mode you specified, and output the name and path of each modified file as well as how many files were modified.    <br><br>
+
+* After the modification has completed, you will be prompted to double check that the changes were made correctly. If they were issues, you can undo all changes made.
--- src/main/java/BatchPropertyEditor.java
+++ src/main/java/BatchPropertyEditor.java
@@ -7,44 +7,47 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 
 public class BatchPropertyEditor {
 
-    public static int numberOfFilesModified = 0;
+    private static Charset charset = StandardCharsets.UTF_8;
+
+    private static int numberOfFilesModified = 0;
+
+    private static HashMap<Path, String> backup = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Welcome to the Bulk Property File Editor!");
-        System.out.println("To get started, please enter the absolute path of your folder to search in (NOT case sensitive) (ex: C:\\Users\\Dave\\someFolder):");
-        System.out.println("NOTE: forward slashes have undefined behavior. Please follow the windows filesystem convention of using backslashes.");
-        String userFolder = reader.readLine();
+        System.out.println("To get started, please enter the absolute path to your folder of AFT's (ex: C:\\Users\\u1339709\\folderToSearchForPropFilesIn):");
+        System.out.println("NOTE: Please follow the windows filesystem convention of using backslashes in your file paths.");
+        String folderPath = reader.readLine();
 
-        while (!userFolder.matches("^(C|c):\\\\(.+ *\\\\?)+$")) {
-            System.out.println("Invalid path entered. Please enter a valid absolute path of your folder to search in (ex: C:\\Users\\Dave\\someFolder):");
-            userFolder = reader.readLine();
+        while (!folderPath.matches("^[a-zA-Z]:\\\\.*$")) {
+            System.out.println("Invalid path entered. Please enter a valid absolute path to your folder of AFT's (ex: C:\\Users\\u1339709\\folderToSearchForPropFilesIn):");
+            folderPath = reader.readLine();
         }
-        if (userFolder.charAt(userFolder.length() -1) == '\\')
-            userFolder = userFolder.substring(0, userFolder.length() - 1);
+        if (folderPath.charAt(folderPath.length() -1) == '\\')
+            folderPath = folderPath.substring(0, folderPath.length() - 1);
 
-        // Create a File object for the directory
-        File userDirectory = new File(userFolder);
-        // Check if the provided path is a directory
-        if (!userDirectory.isDirectory()) {
+        File folder = new File(folderPath);
+        if (!folder.isDirectory()) {
             System.out.println("The directory path you provided was not found. Please rerun the program and try again.");
             System.exit(0);
         }
 
         System.out.println();
-        System.out.println("Please enter the common name of the property files or a regular expression to match by (ex: user.properties to modify files called user.properties or .+.properties to modify files ending with .properties):");
-        String propertyFileRegex = reader.readLine();
+        System.out.println("Please enter the common name of the property files (Supports regex entry such as: .*\\.properties (to match any file ending in .properties) or aft.properties (to match any file with exact name 'aft.properties')):");
+        String propertyFileName = reader.readLine();
 
         System.out.println();
         System.out.println("There are two file modification modes:");
-        System.out.println("1. Setting the value of a given key. This option means you must specify a key and a value. For example, you could specify a key 'username' and a value 'dave', then each property file containing the key 'username' would have its value set to 'dave'.");
-        System.out.println("2. Setting a value given the old value. This option means you must specify an old value and a new value. For example, you could specify an old value 'dave' and a new value 'david', then each property file containing the old value 'dave' would have its value set to 'david'.");
-        System.out.println("NOTE: option 2 can be dangerous if a file has multiple keys with the same value, as they will all be replaced. Be cautious with this option.");
+        System.out.println("1. Setting the value of a given key. This option means you must specify a key and a value. For example, you could specify a key 'db.password' and a value 'myDbPassword', then each property file containing the key 'db.password' would have its value set to 'myDbPassword'.");
+        System.out.println("2. Setting a value given the old value. This option means you must specify an old value and a new value. For example, you could specify an old value 'myOldDbPassword' and a new value 'myNewDbPassword', then each property file containing the old value 'myOldDbPassword' would have that value replaced with 'myNewDbPassword'.");
+        System.out.println("IMPORTANT: Be cautious using option 2 - it can be 'dangerous' if the value you are modifying exists more than once in the same property file, since the program replaces all instances of the value in the file.");
         System.out.println("Please enter the modification mode you would like to use (1 or 2):");
         String option = reader.readLine();
         while (!option.equals("1") && !option.equals("2")) {
@@ -56,37 +59,48 @@ public class BatchPropertyEditor {
         String oldPropValue = "";
         System.out.println();
 
-        switch(option)
-        {
-            case "1" -> {
-                System.out.println("Please enter the key to replace the value for (ex: username):");
-                propKey = reader.readLine();
-            }
-            case "2" -> {
-                System.out.println("Please enter the old property value to replace (ex: dave):");
-                oldPropValue = reader.readLine();
-            }
+        if (option.equals("1")) {
+            System.out.println("Please enter the key to replace the value for (ex: db.password):");
+            propKey = reader.readLine();
+        }
+        else {
+            System.out.println("Please enter the old property value to replace (ex: myOldDbPassword):");
+            oldPropValue = reader.readLine();
         }
 
         System.out.println();
-        System.out.println("Please enter the new property value to set (ex: david):");
+        System.out.println("Please enter the new property value to set (ex: myNewDbPassword):");
         String propNewValue = reader.readLine();
         System.out.println();
 
-        if (option.equals("1"))
-            replacePropertyRecursive(userDirectory, propertyFileRegex, propKey, propNewValue, true);
-        else
-            replacePropertyRecursive(userDirectory, propertyFileRegex, oldPropValue, propNewValue, false);
+        if (option.equals("1")) replacePropertyRecursive(folder, propertyFileName, propKey, propNewValue, true);
+        else replacePropertyRecursive(folder, propertyFileName, oldPropValue, propNewValue, false);
 
         System.out.println();
-        System.out.println("Modification complete. Successfully modified " + numberOfFilesModified + " files. Please check one of the property files to make sure the property was set correctly.");
+        System.out.println("Modification complete. Successfully modified " + numberOfFilesModified + " files.");
+        System.out.println("Please check one of the property files to make sure the property was set correctly.");
+        System.out.println("If you notice an issue in any file, enter either \"u\" or \"undo\" to undo all changes. Otherwise, press any other key to exit the program.");
+        option = reader.readLine();
+        if (option.equalsIgnoreCase("undo") || option.equalsIgnoreCase("u")) {
+            backup.forEach((path, content) -> {
+                try {
+                    Files.writeString(path, content, charset);
+                } catch (IOException e) {
+                    System.out.println("Unknown error undoing changes to:" + path);
+                    System.exit(0);
+                }
+                System.out.println("Reverted file: " + path + " to its original state.");
+            });
+            System.out.println("All changes have been successfully reverted.");
+        }
+        System.out.println("All done! Exiting . . .");
     }
 
-    private static void replacePropertyRecursive(File directory, String propFileName, String propKeyOrOldValue, String newPropValue, boolean replaceByKey) {
+    private static void replacePropertyRecursive(File folder, String propFileName, String propKeyOrOldValue, String newPropValue, boolean replaceByKey) {
         // Get all files and subdirectories in the current directory
-        File[] files = directory.listFiles();
+        File[] files = folder.listFiles();
 
-        if (files != null && !directory.getName().equals(".git")) {
+        if (files != null && !folder.getName().equals(".git")) {
             for (File file : files) {
                 // If it's a directory, then recursively search in that directory
                 if (file.isDirectory()) {
@@ -96,10 +110,10 @@ public class BatchPropertyEditor {
                     if (file.getName().matches(propFileName)) {
                         try {
                             if (replaceByKey)
-                                replaceTextInFile(file, propKeyOrOldValue + " *= *(.+)", propKeyOrOldValue + "=" + newPropValue);
-                            else replaceTextInFile(file, propKeyOrOldValue, newPropValue);
+                                replaceTextInFile(file, propKeyOrOldValue + " *=.*", propKeyOrOldValue + "=" + newPropValue);
+                            else replaceTextInFile(file, "= *" + propKeyOrOldValue, "=" + newPropValue);
                         } catch (IOException e) {
-                            System.out.println("Unknown error occurred writing to file. Please restart the program and try again.");
+                            System.out.println("Unknown error occurred writing to:" + file.getName() + ". Please restart the program and try again.");
                             System.exit(0);
                         }
                     }
@@ -108,14 +122,14 @@ public class BatchPropertyEditor {
         }
     }
 
-    public static void replaceTextInFile(File file, String oldText, String newText) throws IOException {
-        Path path = Paths.get(file.getAbsolutePath());
-        Charset charset = StandardCharsets.UTF_8;
+    public static void replaceTextInFile(File textFile, String oldText, String newText) throws IOException {
+        Path path = Paths.get(textFile.getAbsolutePath());
         String oldContent = Files.readString(path, charset);
         String newContent = oldContent.replaceAll(oldText, newText);
         if (!oldContent.equals(newContent)) {
             numberOfFilesModified++;
-            System.out.println("Modified file: " + file.getAbsolutePath());
+            System.out.println("Modified file: " + textFile.getAbsolutePath());
+            backup.put(path, oldContent);
         }
         Files.writeString(path, newContent, charset);
     }
